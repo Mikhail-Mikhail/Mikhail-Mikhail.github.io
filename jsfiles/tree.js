de9a6e65a9366422bbecf21f868b56491a9ef047
Mikhail-Mikhail <mmbrdf@mail.ru>
2017-05-19 23:07:15 +0400

    ver_19_05_2017(2)

--- jsfiles/tree.js
+++ jsfiles/tree.js
@@ -8,17 +8,40 @@
 
 //---------------------------------------------------------------------------------------
 
-  var elem = document;
+  var elem;// = document;
      
     //Функция перемещения по дереву HTML-элементов.
 
    function TreeSearch() {
    	
+      elem = document.getElementById("bodyTag");
+
      //elem = elem.firstChild;    
 
-      alert(elem); 
-   }
+     // alert("Узел="+elem+"  Тип узла="+elem.nodeType+"  Имя="+elem.nodeName+"  Значение="+elem.nodeValue);
+
+     displayInfo(elem); 
+
+       //if(elem.firstChild) elem = elem.firstChild; 
+
+        //if(elem.nextSibling) elem = elem.nextSibling; 
+
+        if(elem.childNodes) {
+          for(var i=0; i<elem.childNodes.length; i++) {
+             displayInfo(elem.childNodes[i]);
+           }
+        }   
+  }
  
- //---------------------------------------------------------------------------------------  
+ //--------------------------------------------------------------------------------------- 
+  
+  //Функция отображения информации об узле дерева HTML-документа.
+
+  function displayInfo(elem) {
+
+      alert("Узел="+elem+"  Тип узла="+elem.nodeType+"  Имя="+elem.nodeName+"  Значение="+elem.nodeValue);    
+  }
+ 
+ //--------------------------------------------------------------------------------------- 
 
   
