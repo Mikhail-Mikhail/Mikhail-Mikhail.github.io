cf329b29f2d839a53e05b59a4fd41745eb6547c3
Mikhail-Mikhail <mmbrdf@mail.ru>
2017-12-09 23:10:30 +0400

    ver_09_12_2017

--- jsfiles/http.js
+++ jsfiles/http.js
@@ -38,38 +38,33 @@
   // Обработчик событий кнопки.
 
   function BtnEventHandler(){
-//alert("YES!!!");
-//document.getElementById("answer").innerHTML = "YES";
-   var request = new XMLHttpRequest(); // Создать новый HTTP-запрос.
-    
-     // Задать тип запроса и URL-адрес ресурса которому адресован запрос.
-     request.open("GET", "http://localhost:8080/hello2/greetin");
-   
-       //Назначить обработчик события получения ответа на запрос.
-       request.onreadystatechange = function(){
-         //Обработать ответ.
-         ReqHandler(request);
-       // document.getElementById("answer").innerHTML = request.responseText;
-       };
 
-    request.send(null); // Отправить запрос.
+    // Сформировать URL-адрес для формирования Get-запроса.
+    // Параметр "jsonp=CallBackFunction" задает имя функции обратного вызова, которая
+    // будет вызвана при получении ответа на запрос.
+    var url = "http://localhost:8080/hello2/greeting"+"?"+"jsonp=CallBackFunction"
+
+     // Создать элемент <script>. При вставке этого элемента в документ произойдет формирование и 
+     // отправка Http-запроса.
+     var script = document.createElement("script");
+
+      //Задать аттрибут "src" для элемента <script>. 
+      script.src = url; 
+          
+
+    //Вставить элемент <script> в документ. После этого, браузер автоматически выдаст Http-запрос.
+    document.body.appendChild(script); 
   }
 //---------------------------------------------------------------------------------------
  
-   // Обработчик события получения ответа на GET-запрос.
- 
-   function ReqHandler(request){
-alert("Enter!!!");
-     //Если GET-запрос выполнен успешно.
-     //if (request.readyState === 4 && request.status === 200) {
-    if (request.readyState === 4 ) {
-      //Отобразить текст ответа на странице.
-      document.getElementById("answer").innerHTML = request.status;
-      //document.getElementById("answer").innerHTML = request.responseText;
-       alert("request.status="+request.status);
-
-     }  
-  }
+  
+  //Функция обратного вызова, которая будет вызвана при получении ответа на запрос.
+  
+ function CallBackFunction(response){
+         
+    // Отобразить принятые данные на странице.
+    document.getElementById("answer").innerHTML = response;
+  };
 //---------------------------------------------------------------------------------------
 
  
