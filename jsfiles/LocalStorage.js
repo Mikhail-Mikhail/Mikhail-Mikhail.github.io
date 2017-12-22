81265de023e7418d35e3659a2e3a6caca98ffc9f
Mikhail-Mikhail <mmbrdf@mail.ru>
2017-12-22 23:44:24 +0400

    ver_22_12_2017

--- jsfiles/LocalStorage.js
+++ jsfiles/LocalStorage.js
@@ -18,11 +18,12 @@
    
    function PostConstruct(){
 
-      //Найти элемент заголовка по id.
+      //Найти элемент заголовка <h2> по id.
       var HeaderElem = document.getElementById("Header"); 
 
        //Прочитать данные из объекта "localStorage" по ключу "name".
        var name = localStorage.getItem("name"); 
+
         //Прочитать данные из объекта "localStorage" по ключу "surname".
         var surname = localStorage.getItem("surname"); 
 
@@ -32,12 +33,27 @@
              //Отобразить приветствие пользователю.
              HeaderElem.innerText = "Hello "+name+" "+surname+"!";
            }  
-        }   
+        }
+
+         //Прочитать данные из объекта "localStorage" по ключу "MyObj" и
+         //преобразовать их из формата JSON в объект JavaScript.
+         var readObj = JSON.parse(localStorage.getItem("MyObj"));
+         
+          //Если данные прочитаны успешно.
+          if(readObj!==null){
+            //Найти элемент заголовка <h3> по id.
+            var h3Elem = document.getElementById("h3_elem"); 
+
+             //Отобразить прочитанные данные.
+             h3Elem.innerText = "Mr. "+readObj.name+" "+readObj.surname+", your ID="+readObj.id+"!";
+          }  
    }
 
  //--------------------------------------------------------------------------------------
 
+
   //Обработчик кнопки "Сохранить данные".
+
 
   function SaveButtonHandler(){
 
@@ -47,8 +63,32 @@
       //Найти окно для ввода фамилии по id.
       var SurnameElem = document.getElementById("SurnameInputText"); 
 
-       //Сохранить введенные имя и фамилию в объекте localStorage.   
+       //Сохранить введенные имя и фамилию в объекте "localStorage".   
        localStorage.setItem("name", NameElem.value);
        localStorage.setItem("surname", SurnameElem.value);
+
+         //Создать объект.
+         var obj = {
+                      name: NameElem.value,
+                      surname: SurnameElem.value,
+                      id: 839
+                     };
+       
+           //Преобразовать объект в формат JSON.
+           var MyJsonObj = JSON.stringify(obj);         
+
+           //Сохранить объект "MyJsonObj" в объекте "localStorage".   
+           localStorage.setItem("MyObj", MyJsonObj);
+  }
+//--------------------------------------------------------------------------------------
+ 
+
+  //Обработчик кнопки "Удалить данные".
+
+
+  function ClearButtonHandler(){
+
+    //Удалить все данные из объекта "localstorage".
+    localStorage.clear();
   }
 //--------------------------------------------------------------------------------------
