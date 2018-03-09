023dc6494111fc540e843c23e9fbca5895088a2e
Mikhail-Mikhail <mmbrdf@mail.ru>
2018-03-09 22:49:59 +0400

    ver1_09_03_18

--- jsfiles/FileSystem.js
+++ jsfiles/FileSystem.js
@@ -21,20 +21,54 @@
   //Найти элемент для отображения информационных сообщений.
   var Display = document.getElementById("Message");
 //alert("FS supported");
-if(requestFileSystem)  alert("FS supported");  
+/*
+if(window.webkitRequestFileSystem) {
+   alert("FS supported");
+   //Создать файловую систему.
+  // navigator.webkitRequestFileSystem(window.PERSISTENT, 1024, onInitFs, errorHandler);
+}  
+*/
+
+var requestedBytes = 1024*2; 
+
+navigator.webkitPersistentStorage.requestQuota (
+    requestedBytes, function(grantedBytes) {  
+       window.webkitRequestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+
+    }, function(e) { alert("Error"+e); }
+);
+
+/*
+window.storageInfo.requestQuota(PERSISTENT, 1024*1024, 
+    function(grantedBytes) {
+       //Создать файловую систему.
+       window.webkitRequestFileSystem(window.PERSISTENT, grantedBytes, onInitFs, errorHandler);
+    }, 
+    errorHandler);
+*/
+
 /*
    //Создать файловую систему.
-   requestFileSystem(PERSISTENT,
-                     10*1024*1024,  //Размер файловой системы = 10 Мбайт.
-                     function(fs) {  //В случае успешного создания файловой системы вызвать эту функцию.
-                      filesystem = fs; //Сохранить ссылку на файловую систему в глобальной переменной.
-                     },
-                     function(e) {  //В случае ошибки при создании файловой системы вызвать эту функцию.
-                     // alert("При создании файловой системы произошла ошибка...");
-                     } );
-*/
+   window.webkitRequestFileSystem(window.PERSISTENT,
+                                          1,  //Размер файловой системы = 10 Мбайт.
+                                      function(fs) {  //В случае успешного создания файловой системы вызвать эту функцию.
+                                       filesystem = fs; //Сохранить ссылку на файловую систему в глобальной переменной.
+                                      },
+                                      function(e) {  //В случае ошибки при создании файловой системы вызвать эту функцию.
+                                       alert("При создании файловой системы произошла ошибка..."+e);
+                                      } );
+  */
+
  alert("Проверка");
-      //if(filesystem!=null) Display.innerHTML = "Файловая система создана успешно.";
+   //   if(filesystem!=null) Display.innerHTML = "Файловая система создана успешно.";
+
+
+       function errorHandler(e){
+         alert("Error occured: "+e);
+        }
+        function onInitFs(fs){
+         alert("Init OK!");
+        }
 
 /*
       //Обработчик события завершения загрузки выбранного файла.
