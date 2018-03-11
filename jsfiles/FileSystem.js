481e3671ebb0d32567f66df9deaa3df7cc1edbd0
Mikhail-Mikhail <mmbrdf@mail.ru>
2018-03-11 23:49:01 +0400

    ver12_11_08_2018

--- jsfiles/FileSystem.js
+++ jsfiles/FileSystem.js
@@ -29,10 +29,7 @@
       var requestedBytes = 1024*1024; //1Мб. 
 
 
-navigator.webkitPersistentStorage.requestQuota (
-    requestedBytes, SuccessFunction,
-       function(e) { console.log('Error', e); }
-);
+navigator.webkitPersistentStorage.requestQuota(requestedBytes, SuccessFunction, function(e) { console.log('Error', e); });
 
        //Запросить квоту на создание файловой системы требуемого размера.
        //Браузер запросит у пользователя разрешение на создание файловой системы.
